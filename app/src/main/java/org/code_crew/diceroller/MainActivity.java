a37be7f5bc4b5f2904c24b1bc076d916e491d0ec
Christopher Pace <Christopher_pace@ymail.com>
2018-11-23 11:01:28 -0600

    check in

--- app/src/main/java/org/code_crew/diceroller/MainActivity.java
+++ app/src/main/java/org/code_crew/diceroller/MainActivity.java
@@ -2,12 +2,63 @@ package org.code_crew.diceroller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView dice_picture;
+    Random generate = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        imageView = View.findViewById(R.id.imageView);
+        dice_picture = (ImageView) findViewById(R.id.imageView);
+
     }
+
+    public void roll(View view) {
+
+        int diceNum = generate.nextInt(7) + 1;
+        System.out.println(diceNum);
+        switch (diceNum)
+        {
+            case (1):
+                dice_picture.setImageResource(R.drawable.one);
+                break;
+            case (2):
+                dice_picture.setImageResource(R.drawable.two);
+                break;
+            case (3):
+                dice_picture.setImageResource(R.drawable.three);
+                break;
+
+
+
+
+
+
+        }
+
+    }
+
+//    public void Roll(View view){
+//        int myimage[one.png,four.png,five.png,six.png] = new int(6);
+//
+//        int diceNum = generate.nextInt(7);
+//        System.out.println(diceNum);
+//
+//
+//
+//
+//        imageView.setVisible(true);
+//
+//
+//
+
+
+//    }
 }
